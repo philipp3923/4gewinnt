e9b56da8fbd27f1d379060f338cb2f00ca89b11e
Philipp Eichwald <philipp3923@icloud.com>
2022-02-01 11:49:07 +0100

    count_column() und count_row() aus gewonnen() ausgelagert

--- 4gewinnt.c
+++ 4gewinnt.c
@@ -10,13 +10,16 @@ unsigned int feld[X][Y] = {0};
 
 int setzen(unsigned int x, unsigned int spieler);
 int gewonnen(unsigned int spieler);
+int count_row(int x, int spieler);
+int count_column(int y, int spieler);
 void feld_ausgabe();
+
 
 int main(){
 	setzen(1, s1);
-	setzen(2, s1);
-	setzen(3, s1);
-	setzen(4, s1);
+	setzen(1, s1);
+	setzen(1, s1);
+	setzen(1, s1);
 	feld_ausgabe();
 	printf("%i\n",gewonnen(s1));
 }
@@ -41,40 +44,66 @@ int setzen(unsigned int x, unsigned int spieler){
 int gewonnen(unsigned int spieler){
 	//check der y Spalten
 	for(int x = 0; x < X; x++){		
-		int count = 0;	
-		for(int y = 0; y < Y; y++){
-			//printf("[%i, %i]: %i\n", x,y,feld[x][y]);
-			if(feld[x][y] == spieler){
-				count++;
-			}else if(count != 0){
-				break;
-			}
-		}
-		if(count >= 4){
+		if(count_row(x, spieler) >= 4){
 			return 1;
 		}
 	}
 	//check der x Zeilen
 	for(int y = 0; y < Y; y++){		
-		int count = 0;	
-		for(int x = 0; x < X; x++){
-			//printf("[%i, %i]: %i\n", x,y,feld[x][y]);
+		if(count_column(y, spieler) >= 4){
+			return 1;
+		}
+	}
+	
+	//check der Diagonalen
+	for(int i = 1; i < Y; i++){
+		int x = 0;
+		int y = i;
+		int count = 0;
+		while(y < Y && x < X){
 			if(feld[x][y] == spieler){
 				count++;
 			}else if(count != 0){
 				break;
 			}
+			y++;
+			x++;
 		}
 		if(count >= 4){
 			return 1;
 		}
 	}
 	
-	//check der Diagonalen
-	
-	
 	return 0;
 }
+
+int count_row(int x, int spieler){
+	int count = 0;	
+	for(int y = 0; y < Y; y++){
+		if(feld[x][y] == spieler){
+			count++;
+		}else if(count != 0){
+			return count;
+		}
+	}
+	return count;
+}
+
+int count_column(int y, int spieler){
+	int count = 0;	
+	for(int x = 0; x < X; x++){
+		if(feld[x][y] == spieler){
+			count++;
+		}else if(count != 0){
+			return count;
+		}
+	}
+	return count;
+}
+
+
+
+
 
 void feld_ausgabe(){
 	for(int y = Y-1; y >= 0; y--){	
