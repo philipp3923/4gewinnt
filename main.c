bda2eb03eb56ebdafe4cf5bbd30181ebd671708d
Philipp Eichwald <philipp3923@icloud.com>
2022-02-12 12:09:04 +0100

    fehler im gewinncheck muss gefixed werden

--- main.c
+++ main.c
@@ -4,76 +4,57 @@
 
 int main(){
 
-    int x = 0;
-    unsigned char c1;
+
+	int feld[X][Y] = {0};
     
     clear();
     
+    printf("---------------------------------------\n");
+    printf("\n");
+    printf("********* Herzlich Willkommen *********\n");
+    printf("\n");
+    printf("---------------------------------------\n");
     
+    printf("        P R E S S    E N T E R\n");
     printf("\n");
-    printf("\n");
+    printf("********** Drücken Sie Enter **********\n");
     
-    printf("---------------------------------------- \n");
-    printf("\n");
-    printf("********* Herzlich Willkommen ********* \n");
-    printf("\n");
-    printf("---------------------------------------- \n");
-    
-    printf("Drücken Sie Enter um das Spiel zu starten: \n");
-    printf("\n");
-    printf("***************** Drücken Sie Enter ***************** \n");
+    char c1;
     c1=getchar();
     clear();
     
-    int feld[X][Y] = {0};
-    void unten();
+    printf("\n");
+	printf("\n");
+	spielfeld(feld);
+	printf("\n");
+	printf("\n");
+	
+    while(!gewonnen(s2,feld)){
         
-    while(!gewonnen(s1,feld) && !gewonnen(s2,feld)){
+        int x = 0;
         do{
-                printf("Zusetzende Reihe (zwischen 1 und 7): \n");
-                scanf("%d", &x);
+			printf("Wählen einer Reihe zwischen 1 und 7: \n");
+			scanf("%d", &x);
         }while(setzen(x-1, s1, feld));
         
-        gewonnen(s1,feld);
-        
         clear();
-
-    {
+		if(gewonnen(s1, feld)){
+			printf("\n");
+			printf("\n");
+			printf("Computer hat gewonnen.\n");
+			return 0;
+		}
 		int m = move(feld);
 		printf("Der Computer setzt in Reihe: %i\n", m+1);
 		setzen(m, s2, feld);
-    } 
        
-    printf("\n");
-    printf("\n");
-    spielfeld(feld);
-    printf("\n");
-    printf("\n");
+		printf("\n");
+		printf("\n");
+		spielfeld(feld);
+		printf("\n");
+		printf("\n");
     }
     
-    clear();
-    
-    if (gewonnen(s1,feld)==1) {
-        printf("\n");
-        printf("\n");
-        spielfeld(feld);
-        printf("\n");
-        printf("\n");
-        printf("**************************************************** \n");
-        printf("***************** Du hast GEWONNEN ***************** \n");
-        printf("**************************************************** \n");
-        printf("\n");
-    } else {
-        printf("\n");
-        printf("\n");
-        spielfeld(feld);
-        printf("\n");
-        printf("\n");
-        printf("**************************************************** \n");
-        printf("************ Der Computer hat gewonnen ************* \n");
-        printf("**************************************************** \n");
-        printf("\n");
-        
-    }
-
+    printf("Sie haben gewonnen.\n");
+	return 0;
 }
